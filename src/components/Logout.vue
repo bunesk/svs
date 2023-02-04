95bee172fa21b01595ec1c1456f736ccd8fb1fbe
bunesk <mail@bunesk.net>
2023-02-04 22:52:49 +0100

    responsive logout

--- src/components/Logout.vue
+++ src/components/Logout.vue
@@ -18,7 +18,7 @@ const logout = () => {
     @click="logout"
     icon="pi pi-sign-out"
     label="Abmelden"
-    class="p-button-sm "
+    class="p-button-sm"
   />
 </template>
 
@@ -27,5 +27,13 @@ const logout = () => {
   position: absolute;
   top: 0.5rem;
   right: 0.5rem;
+}
+@media only screen and (max-width: 700px) {
+  .p-button {
+    top: 0.3rem;
+    right: 0.3rem;
+    font-size: 0.75rem;
+    padding: 0.375rem 0.5625rem;
+  }
 }
 </style>
